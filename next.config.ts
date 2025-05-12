144ee393ed848adcd88f58af067e9bf83e489252
takeshi-kishima <yahoo.fukuoka@gmail.com>
2025-05-12 18:45:47 +0900

    CI調整その３

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  basePath: process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}` : '',
+  assetPrefix: process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` : '',
+  images: {
+    unoptimized: true, // GitHub PagesではNext.jsの画像最適化がデフォルトでは機能しないため
+  },
+
 };
 
 export default nextConfig;
